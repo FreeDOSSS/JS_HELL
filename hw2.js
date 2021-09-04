f029b2636bd40b5fd1610a28aafb4cd63449f0f3
FreeDOSSS <dima.shkarabura@gmail.com>
2021-09-04 19:37:38 +0300

    feat: hw2 done

---
+++ hw2.js
@@ -0,0 +1,11 @@
+const total = 100;
+const order = 200;
+
+if (order < total) {
+    console.log("Заказ оформлен, с вами свяжется менеджер")
+} 
+
+if (order > total) {
+    console.log("На складе недостаточно товаров!")
+
+}
